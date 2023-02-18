9c5d9ba404eb90085ef2d148d70a04a5da7c6b99
realVinayak <realvinayak123@gmail.com>
2023-02-17 18:12:22 -0600

    Add saving samples in output txt

--- rejection_sampling.py
+++ rejection_sampling.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from histogram_utils import get_histogram_data
+from file_list_utils import write_list
+
 
 N_SAMPLE = 1000000
 
@@ -21,9 +23,11 @@ def rejection_sample_uniform(base_pdf, N_samples, x_min, x_max):
 def driver():
     sin_scaled = lambda x: (math.pi / 2) * math.sin(x * math.pi)
     samples_drawn = rejection_sample_uniform(sin_scaled, N_SAMPLE, 0, 1)
+    write_list(samples_drawn, './outputs/samples.txt')
     probs, bins = get_histogram_data(samples_drawn, N_SAMPLE + 1)
     plt.plot(bins, probs, alpha=0.5, linewidth=0.4)
     plt.savefig('./outputs/sin_hist.png')
+    plt.show()
 
 
 if __name__ == "__main__":
